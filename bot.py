27df60cfaeeaed4704cdf96c2630054a40ddef0a
Max Samsonenko <samsonenko.maxwork@gmail.com>
2025-01-04 16:58:07 +0200

    complete

--- bot.py
+++ bot.py
@@ -1,18 +1,32 @@
 from task_4 import add_contact, change_contact, show_phone, add_birthday, show_birthday, birthdays
 from bot_logic import AddressBook
+import pickle
+
+def save_data(book, filename="addressbook.pkl"):
+    with open(filename, "wb") as f:
+        pickle.dump(book, f)
+
+def load_data(filename="addressbook.pkl"):
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return AddressBook()  # Повернення нової адресної книги, якщо файл не знайдено
+
 
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 def main():
-    book = AddressBook()
+    book = load_data()
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
 
         if command in ["close", "exit"]:
+            save_data(book)
             print("Good bye!")
             break
 
